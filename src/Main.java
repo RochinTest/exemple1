7bb6a98c8572a99ba06e2f5fdbbd179aac449119
Rimlin <Rimlin@gmail.com>
2018-12-23 17:10:54 +0200

    init1

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        int n = 0;
         System.out.println("Введите число n :");
         Scanner sc = new Scanner(System.in);
 
-
         if (sc.hasNextInt()) {
-
-            n = sc.nextInt();
+            int n = sc.nextInt();
             System.out.println("n^0= " + n * 0);
             System.out.println("n^1= " + n * 1);
             System.out.println("n^2= " + n * n);
